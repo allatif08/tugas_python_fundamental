7b7b70352156b33567a9523c6e2ce6cdfa27b01c
Andri <andriabdulaip@gmail.com>
2023-02-26 10:35:43 +0700

    tugas percabangan

---
+++ tugas_python_Fundamental.py
@@ -0,0 +1,20 @@
+print("ibu memberi perintah, Budi pergi ketoko")
+print("Budi menjawab, baik apa yang harus saya lakukan")
+print("Ibu menjawab, kamu beli 1 botol susun jika ada telur beli 6 telur")
+print("Budi pergi ke toko")
+print("Apakah susu ada")
+print("ada")
+print("Budi mengecek stok juga harga susu dan telur ternyata uangnya cukup")
+jumlah_botol_susu = 158
+jumlah_telur = 1678
+
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} butir")
+if jumlah_botol_susu > 0:
+    print("Budi membeli 1 botol susu")
+    if jumlah_telur > 0:
+        print("Budi membeli 6 butir telur")
+    else:
+        print("Budi tidak menjadi beli telur")
+
+
